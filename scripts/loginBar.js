1d23f28a8d94d9ba38ca358f18f0d30b4ff030f0
Martin <martinbruun1995@gmail.com>
2023-10-25 18:05:29 +0200

    Add: login bar js

--- scripts/loginBar.js
+++ scripts/loginBar.js
@@ -1,3 +1,6 @@
+import Pocketbase from 'pocketbase'
+
 const loginBtn = document.querySelector('.login__button')
+const pb = new PocketBase('https://api.martinbruun.dk')
 
 export function userLoggedIn() {}
