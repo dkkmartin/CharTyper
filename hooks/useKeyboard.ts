dac7bd452cfd3c51bd1cf9b1ab7b2fbcc7e20ecd
Martin Bruun <martinbruun1995@gmail.com>
2024-09-04 22:29:40 +0200

    refactor: Improve useKeyboard hook to handle word joining and backspace functionality

--- hooks/useKeyboard.ts
+++ hooks/useKeyboard.ts
@@ -1,20 +1,36 @@
 import { useState, useEffect } from 'react'
 
 function useKeyboard() {
+  const [words, setWords] = useState<string[]>([])
   const [keysPressed, setKeysPressed] = useState<string[]>([])
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Backspace') {
+      if (e.key === ' ') {
+        handleWordJoin()
+      } else if (e.key === 'Backspace') {
         handleBackspace()
       } else {
-        handleKeyPress(e.key)
+        if (keyFilter(e.key)) {
+          handleKeyPress(e.key)
+        }
       }
     }
 
     const handleBackspace = () => {
       if (keysPressed.length > 0) {
+        // Remove the last character from keysPressed
         setKeysPressed(keysPressed.slice(0, -1))
+      } else if (words.length > 0) {
+        // Remove the last character from the last word in words
+        const lastWord = words[words.length - 1].slice(0, -1)
+        if (lastWord === '') {
+          // If the last word becomes an empty string, remove it from words
+          setWords(words.slice(0, -1))
+        } else {
+          // Otherwise, update the last word in words
+          setWords([...words.slice(0, -1), lastWord])
+        }
       }
     }
 
@@ -22,13 +38,40 @@ function useKeyboard() {
       setKeysPressed([...keysPressed, key])
     }
 
+    const handleWordJoin = () => {
+      setWords(
+        words ? [...words, keysPressed.join('')] : [keysPressed.join('')]
+      )
+      setKeysPressed([])
+    }
+
+    const keyFilter = (key: string) => {
+      const ignoredKeys = [
+        'Enter',
+        'Alt',
+        'Control',
+        'AltGraph',
+        'Shift',
+        'Meta',
+        'ArrowUp',
+        'ArrowDown',
+        'ArrowLeft',
+        'ArrowRight',
+        'ContextMenu',
+        'CapsLock',
+        'Tab',
+        'Escape'
+      ]
+      return !ignoredKeys.includes(key)
+    }
+
     document.addEventListener('keydown', handleKeyDown)
     return () => {
       document.removeEventListener('keydown', handleKeyDown)
     }
-  }, [keysPressed])
+  }, [keysPressed, words])
 
-  return keysPressed
+  return { keysPressed, words, setWords }
 }
 
 export default useKeyboard
