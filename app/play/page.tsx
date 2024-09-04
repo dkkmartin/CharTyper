e8b8c53186aa4e329c4da3a810ee09a6e56e424e
Martin Bruun <martinbruun1995@gmail.com>
2024-09-04 18:39:56 +0200

    chore: Add keyboard event handling and sentence generation hooks to Play page

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,5 +1,28 @@
 'use client'
 
+import useKeyboard from '@/hooks/useKeyboard'
+import useSentece from '@/hooks/useSentence'
+import { useEffect, useState } from 'react'
+
 export default function Play() {
-  return <h1>Hey</h1>
+  const keyboard = useKeyboard()
+  const sentence = useSentece(50)
+  const [currentWord, setCurrentWord] = useState<string | null>('')
+
+  useEffect(() => {
+    setCurrentWord(sentence[0])
+  }, [sentence])
+
+  useEffect(() => {
+    console.log(keyboard)
+  }, [keyboard])
+
+  return (
+    <div className="flex flex-col">
+      <section className="w-screen px-4 text-pretty text-center text-3xl text-zinc-500">
+        {sentence.join(' ')}
+      </section>
+      <section>{keyboard && keyboard.join(' ')}</section>
+    </div>
+  )
 }
