0f02fcbfe02430ae3d81275608fd1d8f7207b306
Martin Bruun <martinbruun1995@gmail.com>
2024-09-06 01:29:04 +0200

    refactor: Update Play page to use GameComponent for rendering and keyboard handling

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,49 +1,9 @@
-'use client'
-
-import useKeyboard from '@/hooks/useKeyboard'
-import useSentece from '@/hooks/useSentence'
-import { useEffect, useState } from 'react'
+import GameComponent from '@/components/game/gameComponent'
 
 export default function Play() {
-  const keyboard = useKeyboard()
-  const sentence = useSentece(50)
-  const [currentWord, setCurrentWord] = useState<string>('')
-
-  useEffect(() => {
-    let index = 0
-    if (currentWord === keyboard.words[0]) {
-      keyboard.setWords(keyboard.words.slice(0, 1))
-      index++
-      setCurrentWord(sentence[index])
-    }
-  }, [currentWord, keyboard.words, keyboard, sentence])
-
-  useEffect(() => {}, [])
-
-  useEffect(() => {
-    setCurrentWord(sentence[0])
-  }, [sentence])
-
   return (
     <div className="flex flex-col">
-      <section className="w-screen max-w-[1500px] text-pretty text-center text-3xl text-zinc-500">
-        {sentence.map((word, index) => (
-          <span
-            className={`${
-              currentWord === word ? 'text-white' : 'text-zinc-500'
-            } px-2`}
-            key={index}
-          >
-            {word}{' '}
-          </span>
-        ))}
-      </section>
-      <section>
-        {keyboard.keysPressed.length > 0
-          ? keyboard.keysPressed
-          : keyboard.words.join(' ')}
-      </section>
-      -
+      <GameComponent />
     </div>
   )
 }
