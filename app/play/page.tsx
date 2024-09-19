0b26813af7085a66b101d613cc2af44ec572c83c
Martin <martinbruun1995@gmail.com>
2024-09-19 13:13:00 +0200

    feat: added statistics component

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,8 +1,10 @@
 import GameComponent from '@/components/game/gameComponent'
+import Statistics from '@/components/statistics'
 
 export default function Play() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col px-4">
+      <Statistics />
       <GameComponent />
     </div>
   )
